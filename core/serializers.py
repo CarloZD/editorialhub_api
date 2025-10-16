193045dd2b3ffc913137f392c28cef38dffc9765
CarloZD <carlos.ruiz.l@tecsup.edu.pe>
2025-10-15 22:24:53 -0500

    Implementacion de buscar y eliminar

--- core/serializers.py
+++ core/serializers.py
@@ -1,17 +1,12 @@
 from rest_framework import serializers
 from .models import Editorial, Libro
 
-class LibroSerializer(serializers.ModelSerializer):
-    editorial_nombre = serializers.CharField(source='editorial.nombre', read_only=True)
-
-    class Meta:
-        model = Libro
-        fields = ['id', 'titulo', 'genero', 'anio', 'editorial', 'editorial_nombre']
-
-
 class EditorialSerializer(serializers.ModelSerializer):
-    libros = LibroSerializer(many=True, read_only=True)
-
     class Meta:
         model = Editorial
-        fields = ['id', 'nombre', 'pais', 'libros']
+        fields = ['id', 'nombre', 'pais']
+
+class LibroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Libro
+        fields = ['id', 'titulo', 'genero', 'anio', 'editorial']
