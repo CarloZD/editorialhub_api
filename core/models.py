d197e258b0c181cdaae0e2992ba5873fcde0b12b
CarloZD <carlos.ruiz.l@tecsup.edu.pe>
2025-10-15 21:40:09 -0500

    implementacion de models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Editorial(models.Model):
+    nombre = models.CharField(max_length=100)
+    pais = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    genero = models.CharField(max_length=100)
+    anio = models.PositiveIntegerField()
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE, related_name='libros')
+
+    def __str__(self):
+        return self.titulo
